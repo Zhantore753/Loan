8d784a55cc55da5a1ec15481326302108f55ceb6
Zhantore <753gg@mail.ru>
2020-09-15 17:53:44 +0600

    Слайдер на второй странице

--- src/js/modules/slider/slider-main.js
+++ src/js/modules/slider/slider-main.js
@@ -24,9 +24,7 @@ export default class MainSlider extends Slider {
             } else {
                 this.hanson.classList.remove('slideInUp');
             }
-        } catch (e) {
-            console.log(e);
-        }
+        } catch (e) {}
 
         this.slides.forEach(slide => {
             slide.style.display = 'none';
@@ -39,27 +37,44 @@ export default class MainSlider extends Slider {
         this.showSlides(this.slideIndex += n);
     }
 
-    render() {
-        try {
-            try {
-                this.hanson = document.querySelector('.hanson');
-            } catch (e) {
-                console.log(e);
-            }
-
-            this.btns.forEach(item => {
-                item.addEventListener('click', () => {
-                    this.plusSlides(1);
-                });
-
-                item.parentNode.previousElementSibling.addEventListener('click', (e) => {
-                    e.preventDefault();
-                    this.slideIndex = 1;
-                    this.showSlides(this.slideIndex);
-                });
+    bindTriggers() {
+        this.btns.forEach(item => {
+            item.addEventListener('click', () => {
+                this.plusSlides(1);
             });
 
+            item.parentNode.previousElementSibling.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.slideIndex = 1;
+                this.showSlides(this.slideIndex);
+            });
+        });
+
+        document.querySelectorAll('.prevmodule').forEach(item => {
+            item.addEventListener('click', (e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                this.plusSlides(-1);
+            });
+        });
+
+        document.querySelectorAll('.nextmodule').forEach(item => {
+            item.addEventListener('click', (e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                this.plusSlides(1);
+            });
+        });
+    }
+
+    render() {
+        if (this.container) {
+            try {
+                this.hanson = document.querySelector('.hanson');
+            } catch (e) {}
+
             this.showSlides(this.slideIndex);
-        } catch (e) {}
+            this.bindTriggers();
+        }
     }
 }
